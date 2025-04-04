833610179b5723e116c92c766287a604e29bc1d2
Thomas Ziereis <ziereis@roofline.ai>
2025-04-04 21:14:42 +0200

    enable python auto format

--- init.lua
+++ init.lua
@@ -826,6 +826,7 @@ require('lazy').setup({
         lua = { 'stylua' },
         c = { 'clang-format' },
         cpp = { 'clang-format' },
+        python = { 'black' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
