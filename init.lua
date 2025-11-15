3f4fcd42a88d354e3c7a3e69b07f03a00c91f1fc
Thomas Ziereis <ziereis@roofline.ai>
2025-11-15 10:46:00 +0100

    more colors

--- init.lua
+++ init.lua
@@ -1040,16 +1040,61 @@ require('lazy').setup({
   --     vim.cmd.colorscheme 'tokyonight'
   --   end,
   -- },
+  -- {
+  --   'Tsuzat/NeoSolarized.nvim',
+  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   config = function()
+  --     require('NeoSolarized').setup {
+  --       style = 'dark',
+  --       transparent = true,
+  --     }
+  --     vim.cmd [[ colorscheme NeoSolarized ]]
+  --   end,
+  -- },
+  -- {
+  --   'ellisonleao/gruvbox.nvim',
+  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   config = function()
+  --     require('gruvbox').setup {
+  --       terminal_colors = true, -- add neovim terminal colors
+  --       undercurl = true,
+  --       underline = true,
+  --       bold = true,
+  --       italic = {
+  --         strings = true,
+  --         emphasis = true,
+  --         comments = true,
+  --         operators = false,
+  --         folds = true,
+  --       },
+  --       strikethrough = true,
+  --       invert_selection = false,
+  --       invert_signs = false,
+  --       invert_tabline = false,
+  --       inverse = true, -- invert background for search, diffs, statuslines and errors
+  --       contrast = '', -- can be "hard", "soft" or empty string
+  --       palette_overrides = {},
+  --       overrides = {},
+  --       dim_inactive = false,
+  --       transparent_mode = true,
+  --     }
+  --     vim.cmd [[ colorscheme gruvbox ]]
+  --   end,
+  -- },
   {
-    'Tsuzat/NeoSolarized.nvim',
+    'projekt0n/github-nvim-theme',
+    name = 'github-theme',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      require('NeoSolarized').setup {
-        style = 'dark',
-        transparent = true,
+      require('github-theme').setup {
+        options = {
+          transparent = true,
+        },
       }
-      vim.cmd [[ colorscheme NeoSolarized ]]
+      vim.cmd [[ colorscheme github_dark_dimmed ]]
     end,
   },
 
